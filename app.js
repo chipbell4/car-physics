c078907c414b57a78908bfc679b04be5c9668d97
Chip Bell <cbell@pbs.org>
2016-01-27 22:38:27 -0500

    Adding some basic physics updates

--- app.js
+++ app.js
@@ -28,11 +28,42 @@ var carPhysics = new CarPhysics();
 var lastFrame = Date.now();
 requestAnimationFrame(animate);
 
+var wrap = function(x, a, b) {
+  while(x < a) {
+    x += (b - a);
+  }
+
+  while(x > b) {
+    x -= (b - a);
+  }
+
+  return x;
+}
+
 function animate() {
   var dt = (Date.now() - lastFrame) / 1000;
   lastFrame = Date.now();
 
+  // update car physics based on keyboard
+  carPhysics.thrusterOn = KeyState.up;
+  carPhysics.turnDirection = 0;
+  if(KeyState.left && !KeyState.right) {
+    carPhysics.turnDirection = -1;
+  } else if(KeyState.right && !KeyState.left) {
+    carPhysics.turnDirection = 1;
+  }
+
+  // step the physics sim
   carPhysics.update(dt);
+
+  // clamp the physics simulation position
+  carPhysics.x = wrap(carPhysics.x, 0, viewWidth);
+  carPhysics.y = wrap(carPhysics.y, 0, viewHeight);
+
+  // update the fake car's position and direction
+  graphics.position.x = carPhysics.x;
+  graphics.position.y = carPhysics.y;
+  graphics.rotation = Math.atan2(carPhysics.dy, carPhysics.dx) + Math.PI / 2;
 
   renderer.render(stage);
   requestAnimationFrame(animate);
